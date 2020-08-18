e1024dd7413e29bd18c41ec67a1c4a41f50af82d
Grzegorz Adamczyk <gadamczyk.net@gmail.com>
2020-08-18 21:25:14 +0200

    chore: Create project StackKata.Tests

---
+++ StackKata.Tests/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace StackKata.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+
+        }
+    }
+}
