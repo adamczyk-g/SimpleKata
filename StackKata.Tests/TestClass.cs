4ef5ac16bb63324a95b54b7795358eda5b582bd2
Grzegorz Adamczyk <gadamczyk.net@gmail.com>
2020-08-19 18:10:25 +0200

    chore: create interface IMyStack<T>

--- StackKata.Tests/TestClass.cs
+++ StackKata.Tests/TestClass.cs
@@ -13,4 +13,14 @@ namespace StackKata.Tests
 
         }
     }
+
+    public interface IMyStack<T>
+    {
+        T Pop();
+        void Push(T item);
+        T Peak();
+        void Clear();
+        int Count();
+        bool IsEmpty();
+    }
 }
