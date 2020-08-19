317b6a6b1ebdb5f20e506e9225e945520193e5fd
Grzegorz Adamczyk <gadamczyk.net@gmail.com>
2020-08-19 19:15:39 +0200

    chore: create class MyStack<T>

--- StackKata.Tests/TestClass.cs
+++ StackKata.Tests/TestClass.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace StackKata.Tests
 {
@@ -10,16 +11,26 @@ namespace StackKata.Tests
         {
 
         }
-    }
 
-    public interface IMyStack<T>
-    {
-        T Pop();
-        void Push(T item);
-        T Peak();
-        void Clear();
-        int Count();
-        bool IsEmpty();
-    }
+        public interface IMyStack<T>
+        {
+            T Pop();
+            void Push(T item);
+            T Peak();
+            void Clear();
+            int Count();
+            bool IsEmpty();
+        }
 
+        public class MyStack<T> : IMyStack<T>
+        {
+            public MyStack(){}
+            public T Pop() { throw new NotImplementedException(); }
+            public void Push(T item) { throw new NotImplementedException(); }
+            public T Peak() { throw new NotImplementedException(); }
+            public void Clear() { throw new NotImplementedException(); }
+            public int Count() { throw new NotImplementedException(); }
+            public bool IsEmpty() { throw new NotImplementedException(); }
+        }
+    }
 }
